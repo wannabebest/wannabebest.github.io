6780d693127561472615f7938824592b2e80e64f
Richard Horvath <wannabebest96@gmail.com>
2023-05-20 21:44:24 +0200

    updating some futures

---
+++ js/app.js
@@ -0,0 +1,27 @@
+/* --------------- Grab elements from DOM --------------- */
+
+/* --------------- Sticky Navbar --------------- */
+const header = document.querySelector("header")
+
+function stickyNavbar() {
+  header.classList.toggle("scrolled", window.pageYOffset > 0);
+}
+
+window.addEventListener("scroll", stickyNavbar)
+/* --------------- Reveal Animation --------------- */
+
+/* --------------- Skills Progress Bar Animation --------------- */
+
+/* --------------- Services Counter Animation --------------- */
+
+/* --------------- Portfolio Filter Animation --------------- */
+
+/* --------------- Modal Pop Up Animation Animation --------------- */
+
+/* --------------- Modal Pop Up Animation Animation --------------- */
+
+/* --------------- Change Active Link On Scroll --------------- */
+
+/* --------------- Change Page Theme --------------- */
+
+/* --------------- Open & Close Navbar Menu --------------- */
